e1fc84e02036e909eb989ddc9b189474027e54a8
Groyax <visseauxbe@yahoo.fr>
2015-01-23 11:20:17 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,32 +5,31 @@
 makeCacheMatrix <- function(x = matrix()) {
 	inverse_mat <- NULL
 	setMatrix <- function(y) {
-		x <<- y			#load the matrix in x
-		inverse_mat <<- NULL		#create the variable for the inverse of x at next steps
+		x <<- y				
+		inverse_mat <<- NULL		
 	}
-	getMatrix <- function() x		#view the initial matrix
-	setInverse <- function(initial_mat){
-		inverse_mat <<- solve(initial_mat)	#load the inverse matrix in global environment
-	}
-	getInverse <- function() inverse_mat	#view the inverse matrix
+	getMatrix <- function() x		
+	setInverse <- function(inverse) inverse_mat <<- inverse 
+	getInverse <- function() inverse_mat	
 	list(setMatrix = setMatrix,
 		getMatrix = getMatrix,
 		setInverse = setInverse,
-		getInverse = getInverse)	#create the list of function
+		getInverse = getInverse)	
 }
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
-## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve
+## If the inverse has already been calculated (and the matrix has not been changed), then the cachesolve
 ## retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-	inverse_mat <- x$getInverse()
-	
-	if(!is.null(inverse_mat)){
+	inverse_mat <- x$getInverse()		#stock the inverse matrix saved
+	if(!is.null(inverse_mat) & identical(initial_mat,x$getMatrix())){
+		#verification that a matrix inverse has already been calculated
+		#and that the initial matrix has not changed (global envt == makeCacheMatrix envt) 
 		message("getting cached data")
 		return(inverse_mat)
 	}
-
+	initial_mat <<- x$getMatrix()		#stock the initial matrix in global environment
 	data <- x$getMatrix()
 	inverse_mat <- solve(data, ...)
 	x$setInverse(inverse_mat)
